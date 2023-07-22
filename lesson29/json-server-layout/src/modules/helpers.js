b75b2ad3c34c6734420eff43a2307f200744b06c
likvidov <likvidya@gmail.comm>
2023-07-22 18:59:07 +0300

    Оптимизация работы

--- lesson29/json-server-layout/src/modules/helpers.js
+++ lesson29/json-server-layout/src/modules/helpers.js
@@ -7,3 +7,12 @@ export const debounce = (func, ms = 300) => {
     timer = setTimeout(() => { func.apply(this, args) }, ms);
   }
 }
+
+export const isEmpty = (obj) => {
+  for (const prop in obj) {
+    if (Object.hasOwn(obj, prop)) {
+      return false;
+    }
+  }
+  return true;
+}
--- lesson29/json-server-layout/src/modules/userService.js
+++ lesson29/json-server-layout/src/modules/userService.js
@@ -1,58 +1,67 @@
 export class UserService {
+  constructor() {
+    this.url = 'http://localhost:4545/users';
+  }
+
+  async getData(url) {
+    try {
+      const response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  }
+
+  async sendData(url, user, method) {
+    try {
+      const response = await fetch(url, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: user
+      });
+      return await response.json();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  //Get Data
   getUsers() {
-    return fetch('http://localhost:4545/users')
-      .then(res => res.json())
-  }
-
-  addUser(user) {
-    return fetch('http://localhost:4545/users', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(user)
-    }).then(res => res.json())
-  }
-
-  removeUser(id) {
-    return fetch(`http://localhost:4545/users/${id}`, {
-      method: 'DELETE'
-    }).then(res => res.json())
-  }
-
-  changeUser(id, data) {
-    return fetch(`http://localhost:4545/users/${id}`, {
-      method: 'PATCH',
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      },
-    }).then(res => res.json())
+    return this.getData(this.url)
   }
 
   getUser(id) {
-    return fetch(`http://localhost:4545/users/${id}`).then(res => res.json())
-  }
-
-  editUser(id, user) {
-    return fetch(`http://localhost:4545/users/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify(user),
-      headers: {
-        "Content-Type": "application/json"
-      },
-    }).then(res => res.json())
+    return this.getData(`${this.url}/${id}`);
   }
 
   filterUsers(filterOption) {
-    return fetch(`http://localhost:4545/users?${filterOption}=true`).then(res => res.json())
+    return this.getData(`${this.url}?${filterOption}=true`);
   }
 
   getSortUsers(sortOption) {
-    return fetch(`http://localhost:4545/users?_sort=${sortOption.name}&_order=${sortOption.value}`).then(res => res.json())
+    return this.getData(`${this.url}?_sort=${sortOption.name}&_order=${sortOption.value}`);
   }
 
   getSearchUsers(str) {
-    return fetch(`http://localhost:4545/users?name_like=${str}`).then(res => res.json())
+    return this.getData(`${this.url}?name_like=${str}`);
+  }
+
+  //Send Data
+  addUser(user) {
+    return this.sendData(this.url, JSON.stringify(user), 'POST')
+  }
+
+  removeUser(id) {
+    return this.sendData(`${this.url}/${id}`, null, 'DELETE')
+  }
+
+  changeUser(id, data) {
+    return this.sendData(`${this.url}/${id}`, JSON.stringify(data), 'PATCH')
+  }
+
+  editUser(id, user) {
+    return this.sendData(`${this.url}/${id}`, JSON.stringify(user), 'PUT')
   }
 }
